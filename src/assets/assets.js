f06cb7e718131a68a21e3ddcb9aefba2f242429d
chiscookeke11 <chiscookeke11@gmail.com>
2025-01-17 15:15:36 -1100

    ADD:  Home page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,48 @@
-const icons = {
-    notificationBell: "https://res.cloudinary.com/dwedz2laa/image/upload/v1737113146/notifications_vffj72.svg",
-  };
+export const icons = [{
+    notificationBell: "https://res.cloudinary.com/dwedz2laa/image/upload/v1737113146/notifications_vffj72.svg"
+  }];
   
-  export default icons;
+
+  export const services = [
+    {
+      name: "Ship Parcel",
+      image: "https://res.cloudinary.com/dwedz2laa/image/upload/v1737158014/parcel_a3nfpe.svg",
+    },
+    {
+      name: "Logistic",
+      image: "https://res.cloudinary.com/dwedz2laa/image/upload/v1737158014/logistic_gmwaq6.svg",
+    },
+    {
+      name: "International",
+      image: "https://res.cloudinary.com/dwedz2laa/image/upload/v1737158014/international_xwazkg.svg",
+    },
+    {
+      name: "Bulk Shipping",
+      image: "https://res.cloudinary.com/dwedz2laa/image/upload/v1737158014/bulk_shipping_iaq7ui.svg",
+    }
+  ];
+
+  export const shippingDetails = [
+    {
+      trackingId: "#5R9G87R",
+      date: "14 may 2023",
+      from: "1234 Elm Street Springfield, IL 62701",
+      to: "5678 Maple Avenue Seattle, WA 98101",
+      status: "Delivered"
+    },
+    {
+      trackingId: "#8T9G88P",
+      date: "14 may 2023",
+      from: "1234 Elm Street Springfield, IL 62701",
+      to: "5678 Maple Avenue Seattle, WA 98101",
+      status: "Cancelled"
+    },
+    {
+      trackingId: "#6G9G87R",
+      date: "14 may 2023",
+      from: "1234 Elm Street Springfield, IL 62701",
+      to: "5678 Maple Avenue Seattle, WA 98101",
+      status: "Failed"
+    }
+  ]
   
--- src/pages/Layouts/AppLayout/Home.jsx
+++ src/pages/Layouts/AppLayout/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import SplashScreen from '../../../components/SplashScreen';
-import icons from '../../../assets/assets'; 
-import { FaBell } from 'react-icons/fa6';
+import { icons, services } from '../../../assets/assets';
+import { MdFilterList } from 'react-icons/md';
+import ShippingHistory from '../../../components/ShippingHistory';
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
@@ -21,7 +22,7 @@ const Home = () => {
   } else {
     return (
       <div className="w-full min-h-screen bg-[#21252C]">
-        <div className="flex flex-col gap-8 py-10 px-7">
+        <div className="flex flex-col gap-8 py-10 px-7 mt-2">
           <span className="w-full flex flex-row items-center justify-between">
             <img
               src="https://res.cloudinary.com/dwedz2laa/image/upload/v1736589441/Logo_srltbt.svg"
@@ -30,26 +31,74 @@ const Home = () => {
             />
             <span>
               <img
-                src={newMessage.length > 0 ? icons.notificationBell : icons.notificationBell }
+                src={newMessage.length > 0 ? icons.notificationBell : icons.notificationBell}
                 alt="notification bell"
               />
             </span>
           </span>
 
-          <span>
+          <span className='mb-3' >
             <h1 className='text-[#FFFFFF] text-2xl font-bold text-left mb-2 ' >Track your parcel</h1>
             <p className='text-[#CFD5DB] text-sm font-normal ' >Enter your parcel tracking number to track your parcel live</p>
           </span>
 
-          <div className='bg-white flex w-full flex-row px-4 rounded-lg items-center  ' >
+          <div className='bg-white flex w-full flex-row px-4 rounded-lg items-center  gap-3 ' >
 
-            <span className='w-[10%] ' ><img src="https://res.cloudinary.com/dwedz2laa/image/upload/v1737113217/location_searching_f0owft.svg" alt="pointer" /> </span>
+            <span className='w-[10%] border-r border-[#D2D6DB] pr-3   ' ><img src="https://res.cloudinary.com/dwedz2laa/image/upload/v1737113217/location_searching_f0owft.svg" alt="pointer" className="w-[20px] h-[20px]" /> </span>
 
-            <input type="text" className='w-full px-2 py-4 outline-none border-none'  />
+            <input type="text" className='w-full px-2 py-4 outline-none border-none' />
 
-            <span className='w-[10%]  cursor-pointer'><img src="https://res.cloudinary.com/dwedz2laa/image/upload/v1737113171/QRcode_pamm9x.svg " alt="" /></span>
+            <span className='w-[10%]  cursor-pointer'><img src="https://res.cloudinary.com/dwedz2laa/image/upload/v1737113171/QRcode_pamm9x.svg " alt="" className="w-[20px] h-[20px]" /></span>
           </div>
         </div>
+
+
+
+
+
+        <div className="container bg-white w-full flex flex-col  ">
+          <div className="services mt-4 pt-3 py-2 ">
+            <h1 className='px-6 text-[#21252C] text-lg font-bold ' >Our Services</h1>
+
+
+            <div className='w-full flex flex-row items-center overflow-x-auto mt-8 px-6 gap-8 ' >
+
+              {services.map((service, index) => (
+                <button className='flex items-center justify-center flex-col gap-3  border-none outline-none ' >
+                  <div className='w-[72px] h-[72px] bg-[#F1F2F6] rounded-2xl flex items-center justify-center ' >
+                    <img src={service.image} alt="" />
+                  </div>
+                  <h2 className='text-xs font-semibold text-[#21252C] text-center w-full whitespace-nowrap ' >{service.name} </h2>
+                </button>
+              ))}
+
+            </div>
+          </div>
+
+
+
+          <div className="history-wrapper  mt-6 pt-3 py-2"> 
+              <h1 className='px-5 text-[#21252C] text-lg font-bold w-full flex flex-row justify-between ' > <span>Shipping History</span>     <button> <MdFilterList/> </button></h1>
+
+<ShippingHistory/>
+        
+
+            
+
+          </div>
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
       </div>
     );
   }
